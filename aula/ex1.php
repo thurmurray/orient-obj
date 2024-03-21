5d06cf3143ae2fe7ed5a2e80e42e1d10cf57fc1a
Arthur di Murray <134729312+thurmurray@users.noreply.github.com>
2024-03-21 11:16:56 -0300

    ex1.php

---
+++ aula/ex1.php
@@ -0,0 +1,9 @@
+<?php
+    echo "10 numeros, pulados de dois em dois: \n";
+    $num = 1;
+    for ($lim = 0; $lim < 10; $lim++) { 
+        echo "$num ";
+        $num += 2;
+    }
+    echo "\n";
+?>
